23d5e13faffd428a5038e23d8e81448551ba0b6d
udit <yudistrange@gmail.com>
2023-07-12 18:18:00 +0530

    Add basic skaffolding for a bevy game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use bevy::{
+    prelude::{App, Commands, Component, Plugin, PreStartup, Query, Startup, Update, With},
+    DefaultPlugins,
+};
+
 fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
+fn say_hello() {
     println!("Hello, world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PreStartup, add_people)
+            .add_systems(Startup, say_hello)
+            .add_systems(Update, greet_people);
+    }
 }
